c29a1c68183c2d10710b2553efd4d2a55e6f6d39
Noctunus <alexander.koch@chain4travel.com>
2024-09-20 13:54:10 +0200

    Changed the create_c4t_file_listing.sh to directly output the c4t files to be used directly by the dependency resolution script.

--- scripts/create_c4t_file_listing.sh
+++ scripts/create_c4t_file_listing.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Simple helper script to just create a file which contains a listing of all the cmp proto files in the c4t branch
-# This can then be used by the dependency checker to determine whether a new version of a proto file can be reused
+# This is used by the dependency checker to determine whether a new version of a proto file can be reused
 # when it's not present in the c4t branch
 
-OUTFILE=${1:-temp/c4tfiles.txt}
-git ls-tree -r --name-only origin/c4t proto/cmp | grep -oP "cmp/.*\.proto" > $OUTFILE
+git ls-tree -r --name-only origin/c4t proto/cmp | grep -oP "cmp/.*\.proto" 
