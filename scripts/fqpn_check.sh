e4c4eb31f809d3e55969d2ca9bf8721af1678045
Noctunus <alexander.koch@chain4travel.com>
2024-09-24 12:29:33 +0200

    Included all build-in types of protobuf as an exclusion for the fqpn checker

--- scripts/fqpn_check.sh
+++ scripts/fqpn_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep --include="*.proto" -r -h -oP "(\S+) (\S+) = \d+;" proto/ | cut -d" " -f1 | sort -u | grep -v "google.protobuf" | grep -v "cmp\." | grep -vP "(bool|bytes|double|int32|int64|string|uint64|//)"
+grep --include="*.proto" -r -h -oP "(\S+) (\S+) = \d+;" proto/ | cut -d" " -f1 | sort -u | grep -v "google\.protobuf" | grep -v "cmp\." | grep -vP "(double|float|int32|int64|uint32|uint64|sint32|sint64|fixed32|fixed64|sfixed32|sfixed64|bool|string|bytes|//)"
 
 if [ $? != 1 ] ; then
 	echo "Error: Found non FQPN types!"
