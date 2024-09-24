b0f0dab6cfdc2ce401f928fdfdc194ace6304471
Noctunus <alexander.koch@chain4travel.com>
2024-09-24 12:39:55 +0200

    typo

--- scripts/fqpn_check.sh
+++ scripts/fqpn_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "🔍 Checking if all the types used in the proto files are using a FQPN (full qualified package name)"
+echo -e "🔍 Checking if all the types used in the proto files are using a FQPN (fully qualified package name)"
 
 grep --include="*.proto" -r -h -oP "(\S+) (\S+) = \d+;" proto/ | cut -d" " -f1 | sort -u | grep -v "google\.protobuf" | grep -v "cmp\." | grep -vP "(double|float|int32|int64|uint32|uint64|sint32|sint64|fixed32|fixed64|sfixed32|sfixed64|bool|string|bytes|//)"
 
